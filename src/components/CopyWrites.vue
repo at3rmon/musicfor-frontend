8f22620ddb902d1b0b2788f2a0229031e69a9b77
Christos Boukouvalas <christos.boukouvalas.90@gmail.com>
2022-10-26 15:01:26 +0300

    Code Cleanup

---
+++ src/components/CopyWrites.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { computed } from "vue";
+const appTitle = import.meta.env.VITE_APP_TITLE;
+const fromDate = import.meta.env.VITE_APP_FROM_DATE;
+const toDate = computed(() => {
+  return new Date().getFullYear();
+});
+</script>
+
+<template>&copy; {{ fromDate }}-{{ toDate }} {{ appTitle }}</template>
---
+++ src/components/shared/DefaultFooter.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Copywrites from "@/components/Copywrites.vue";
+</script>
+
+<template>
+  <footer>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="20"
+      height="20"
+    />
+
+    <div class="wrapper">
+      <Copywrites />
+    </div>
+  </footer>
+</template>
